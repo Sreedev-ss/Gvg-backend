c8a956354b41d1dc2dd3c342f836c4abd610bffc
Sreedev-ss <sreedevkply2004@gmail.com>
2024-01-05 01:13:34 +0530

    config

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ connectDB();
 const server = serverConfig()
 const httpMsg = httpStatus()
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'50mb'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'))
