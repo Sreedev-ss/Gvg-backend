f44c43ec529b7a00f95144a4f7fea88eb49b205e
Sreedev-ss <sreedevkply2004@gmail.com>
2024-01-05 00:58:20 +0530

    plant api delete

--- src/controllers/plantController.js
+++ src/controllers/plantController.js
@@ -27,7 +27,18 @@ const createPlant = async (req, res) => {
     }
 }
 
+const deletePlant = async (req, res) => {
+    try {
+        const id = req.params.id
+        const response = await PlantModal.findByIdAndDelete(id)
+        res.json({ message: 'Plant deleted successfully', response })
+    } catch (error) {
+        res.status(500).json({ message: httpMsg[500], error: error });
+    }
+}
+
 module.exports = {
     allPlant,
-    createPlant
+    createPlant,
+    deletePlant
 }
